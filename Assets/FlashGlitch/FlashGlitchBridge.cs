e4772ebbcc4567e05e242e844fb56e057147b1e3
Keijiro Takahashi <keijiro@gmail.com>
2022-06-29 23:43:51 +0900

    VZO integration

---
+++ Assets/FlashGlitch/FlashGlitchBridge.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Video;
+using FlashGlitch;
+
+namespace BibcamStage {
+
+public sealed class FlashGlitchBridge : MonoBehaviour
+{
+    [SerializeField] VideoPlayer _source = null;
+    [SerializeField, Range(0, 1)] float _randomize = 0.2f;
+
+    public void OnNoteChannel0(int note, float velocity)
+      => OnTrigger(0, velocity);
+
+    public void OnNoteChannel1(int note, float velocity)
+      => OnTrigger(1, velocity);
+
+    void OnTrigger(int index, float value)
+    {
+        if (value == 0 || _controller == null) return;
+        value *= Random.Range(1 - _randomize, 1.0f);
+        _controller.TriggerEffect(index, value);
+        _controller.RandomizeSeed();
+        if (Random.value < 0.01f) _controller.RandomizeHue();
+    }
+
+    FlashGlitchController _controller;
+
+    void Start()
+      => _controller = GetComponent<FlashGlitchController>();
+
+    void Update()
+    {
+        if (_source.texture == null) return;
+        _controller.Source = _source.texture;
+    }
+}
+
+} // namespace BibcamStage
